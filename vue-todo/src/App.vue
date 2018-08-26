66b298362e6355997816bec4543fce6f781559aa
es5es5 <es5e5s@nate.com>
2018-08-26 20:25:22 +0900

    saved input text in local storage(feat. #2).

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="app">
-    <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
-  </div>
+<div id="app">
+  <TodoHeader></TodoHeader>
+  <TodoInput></TodoInput>
+  <TodoList></TodoList>
+  <TodoFooter></TodoFooter>
+</div>
 </template>
 
 <script>
@@ -15,13 +15,27 @@ import TodoFooter from './components/TodoFooter.vue'
 
 export default {
   components: {
-    'TodoHeader' : TodoHeader,
-    'TodoInput' : TodoInput,
-    'TodoList' : TodoList,
-    'TodoFooter' : TodoFooter
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
   }
 }
 </script>
 
 <style lang="css">
+body {
+  text-align: center;
+  background-color: #F6F6F8;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,11 +1,21 @@
 <template>
-<div>header</div>
+<header>
+  <h1>TODO it!</h1>
+</header>
 </template>
+
+<style scoped>
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
 
 <script>
 export default {
 }
 </script>
 
-<style lang="css">
+<style>
 </style>
