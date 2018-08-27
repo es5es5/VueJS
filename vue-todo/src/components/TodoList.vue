a9dfe8cf4d9b954f57622f2ebc83534a44099b70
es5es5 <es5e5s@nate.com>
2018-08-27 23:58:03 +0900

    modified TodoList compnents

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,11 +1,63 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo">
+          <i class="fa fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      todoItems : []
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++){
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  methods: {
+    removeTodo() {
+      console.log('clicked');
+    }
+  }
 }
 </script>
 
 <style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62ACDE;
+  margin-right: 5px;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #DE4343;
+}
 </style>
